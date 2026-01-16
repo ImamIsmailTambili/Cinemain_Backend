475e7272612f07786cd8ffd09c5ebd269d9ebadf
ImamIsmailTambili <71882400+ImamIsmailTambili@users.noreply.github.com>
2026-01-16 19:20:39 +0700

    Update generateTokenAdmin.js

--- src/admin/utils/generateTokenAdmin.js
+++ src/admin/utils/generateTokenAdmin.js
@@ -9,7 +9,7 @@ export const generateTokenAdmin = (adminId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
