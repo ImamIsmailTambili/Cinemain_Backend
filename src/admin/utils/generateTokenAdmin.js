45bffb585524e10727f9c2505e35e2f28299a806
ImamIsmailTambili <71882400+ImamIsmailTambili@users.noreply.github.com>
2026-01-16 20:12:26 +0700

    Update cookie settings for JWT token

--- src/admin/utils/generateTokenAdmin.js
+++ src/admin/utils/generateTokenAdmin.js
@@ -9,8 +9,7 @@ export const generateTokenAdmin = (adminId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "lax",
-        domain: ".vercel.app",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
