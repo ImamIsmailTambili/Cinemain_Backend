91c8fdf2f28a3f656a6e8284d49a5dca4f1f3a4d
ImamIsmailTambili <71882400+ImamIsmailTambili@users.noreply.github.com>
2026-01-16 21:41:49 +0700

    Remove httpOnly flag from JWT cookie
    Removed httpOnly flag from JWT cookie settings.

--- src/admin/utils/generateTokenAdmin.js
+++ src/admin/utils/generateTokenAdmin.js
@@ -7,7 +7,6 @@ export const generateTokenAdmin = (adminId, res) => {
     });
 
     res.cookie("jwt", token, {
-        httpOnly: true,
         secure: true,
         sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
