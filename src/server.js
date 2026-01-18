e67b499d17b20ffe38c221d26d37d9ccb197c16d
ImamIsmailTambili <71882400+ImamIsmailTambili@users.noreply.github.com>
2026-01-18 21:53:58 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,10 +26,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: {
+  origin: [
     "https://cinemain-admin.up.railway.app",
     "https://cinemain.up.railway.app"
-  }
+  ]
   credentials: true
 }));
 
